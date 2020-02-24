a280423aee83879a351c42f35082f519d7e4eacb
KOREApsh <tmdgks22222@gmail.com>
2020-02-24 18:07:05 +0900

    initial commit for branch named parksh

--- GalleyDecodeApp/app/src/main/java/com/f10company/galleydecodeapp/MainActivity.java
+++ GalleyDecodeApp/app/src/main/java/com/f10company/galleydecodeapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //initial commit for branch named parksh
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
