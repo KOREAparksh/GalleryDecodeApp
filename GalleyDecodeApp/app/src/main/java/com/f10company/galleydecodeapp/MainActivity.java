b387dc762ff66c2b356059f3dccc2dfc0d896a02
KOREApsh <tmdgks22222@gmail.com>
2020-02-27 22:06:30 +0900

    [add] add google Ads

--- GalleyDecodeApp/app/src/main/java/com/f10company/galleydecodeapp/MainActivity.java
+++ GalleyDecodeApp/app/src/main/java/com/f10company/galleydecodeapp/MainActivity.java
@@ -6,13 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     Button button;
+    private AdView mAdView;
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +54,29 @@ public class MainActivity extends AppCompatActivity {
 
         textView=(TextView)findViewById(R.id.textview);
         button = (Button)findViewById(R.id.button);
+        //mAdView = (AdView)findViewById(R.id.adView);
+        relativeLayout = (RelativeLayout)findViewById(R.id.mainRelativeLayout);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+
+            }
+        });
+
+        //mAdView.setAdSize(new AdSize());
+        //AdRequest adRequest = new AdRequest.Builder().build();
+
+        //AdSize adSize = getAdSize();
+        /*mAdView.setAdSize(adSize);
+        mAdView.loadAd(adRequest);*/
+        mAdView = new AdView(this);
+        mAdView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+        RelativeLayout.LayoutParams rp = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        rp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        mAdView.setLayoutParams(rp);
+        relativeLayout.addView(mAdView);
+        loadBanner();
 
         setSize();
 
@@ -50,16 +84,43 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");*/
                 Intent intent = new Intent(MainActivity.this,DecodeGalleryActivity.class);
 
                 startActivityForResult(intent, 1);
             }
         });
+    }
 
+    private void loadBanner() {
+        // Create an ad request. Check your logcat output for the hashed device ID
+        // to get test ads on a physical device, e.g.,
+        // "Use AdRequest.Builder.addTestDevice("ABCDE0123") to get test ads on this
+        // device."
+        AdRequest adRequest =
+                new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                        .build();
 
+        AdSize adSize = getAdSize();
+        // Step 4 - Set the adaptive ad size on the ad view.
+        mAdView.setAdSize(adSize);
+
+        // Step 5 - Start loading the ad in the background.
+        mAdView.loadAd(adRequest);
+    }
+
+    private AdSize getAdSize() {
+        // Step 2 - Determine the screen width (less decorations) to use for the ad width.
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+
+        float widthPixels = outMetrics.widthPixels;
+        float density = outMetrics.density;
+
+        int adWidth = (int) (widthPixels / density);
+
+        // Step 3 - Get adaptive ad size and return for setting on the ad view.
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
     }
 
     @Override
